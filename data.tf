89c36487f1a1d52f42bc473ae481f42b1311bb36
praveenarupi <praveenarupi@gmail.com>
2022-10-23 09:35:25 +0530

    updating ./terraform-modules/tf-module-mutable-app

--- data.tf
+++ data.tf
@@ -3,7 +3,7 @@ data "aws_ssm_parameter" "ssh_credentials" {
 }
 
 data "aws_ami" "ami" {
-  most_recent   = true
-  name_regex    = "centos7-devops-practice-with-ansible"
-  owners = ["self"]
+  most_recent = true
+  name_regex  = "centos7-devops-practice-with-ansible"
+  owners      = ["self"]
 }
