09e401f4595c2afd7f7884f8c8406f98c068360b
praveenarupi <praveenarupi@gmail.com>
2022-10-25 13:15:30 +0530

    updating ./terraform-modules/tf-module-mutable-app

--- data.tf
+++ data.tf
@@ -7,3 +7,13 @@ data "aws_ami" "ami" {
   name_regex  = "centos7-devops-practice-with-ansible"
   owners      = ["self"]
 }
+
+data "terraform_remote_state" "infra" {
+  backend = "s3"
+
+  config {
+    bucket = "terraform-b66"
+    key    = "mutable/infra/${var.ENV}/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "ondemand" {
   count                  = var.INSTANCES["ONDEMAND"].instance_count
   instance_type          = var.INSTANCES["ONDEMAND"].instance_type
   ami                    = data.aws_ami.ami.image_id
+  subnet_id = data.terraform_remote_state.infra.outputs.app_subnets[count.index]
 }
 
 resource "aws_spot_instance_request" "spot" {
