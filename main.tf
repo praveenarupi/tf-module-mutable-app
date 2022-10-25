48b3974e751ac142824acaac8268b4b65901a1ed
praveenarupi <praveenarupi@gmail.com>
2022-10-25 13:17:26 +0530

    updating ./terraform-modules/tf-module-mutable-app

--- main.tf
+++ main.tf
@@ -9,5 +9,6 @@ resource "aws_spot_instance_request" "spot" {
   count                  = var.INSTANCES["SPOT"].instance_count
   instance_type          = var.INSTANCES["SPOT"].instance_type
   ami                    = data.aws_ami.ami.image_id
+  subnet_id = data.terraform_remote_state.infra.outputs.app_subnets[count.index]
   wait_for_fulfillment   = true
 }
