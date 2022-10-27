f94f9190e21560f4f37323a640b66f2e3275b7db
praveenarupi <praveenarupi@gmail.com>
2022-10-27 19:43:14 +0530

    updating ./terraform-modules/tf-module-mutable-app

--- tg.tf
+++ tg.tf
@@ -1,8 +1,8 @@
 resource "aws_lb_target_group_attachment" "tg" {
   count            = length(local.ALL_INSTANCE_IDS)
-  target_group_arn =  data.terraform_remote_state.infra.outputs.public_tg_arn
+  target_group_arn =  var.COMPONENT == "frontend" ? data.terraform_remote_state.infra.outputs.public_tg_arn : aws_lb_target_group.tg.arn
   target_id        = local.ALL_INSTANCE_IDS[count.index]
-  port             = 80
+  port             = var.APP_PORT
 }
 
 resource "aws_lb_target_group" "tg" {
@@ -10,6 +10,7 @@ resource "aws_lb_target_group" "tg" {
   port                 = var.APP_PORT
   protocol             = "HTTP"
   vpc_id               = data.terraform_remote_state.infra.outputs.vpc_id
+  deregistration_delay = 0
   health_check {
     enabled = true
     healthy_threshold = 2
@@ -17,6 +18,6 @@ resource "aws_lb_target_group" "tg" {
     timeout = 4
     port = var.APP_PORT
     unhealthy_threshold = 2
-    
+
   }
 }
